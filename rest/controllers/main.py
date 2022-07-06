e9480f1482d25d5be3bfb06534ae0aceec52fc48
nimi-odoo <nimi@odoo.com>
2022-07-06 14:06:28 -0400

    [REM] Removed comments and unneeded imports

--- rest/controllers/main.py
+++ rest/controllers/main.py
@@ -4,14 +4,12 @@ from odoo import http
 from odoo.http import request
 
 import json
-from werkzeug.wrappers import Request, Response
 
 class RestController(http.Controller):
     @http.route('/api/<string:str>', auth="public")
     def index(self, **kw):
-        # headers = {"Content-Type":"application/json"}
         headers = [("Content-Type", "application/json")]
-        print(headers)
+        print("Headers\n", headers)
 
         url_path = kw["str"]
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
@@ -22,18 +20,8 @@ class RestController(http.Controller):
         print(f"\napi {url_path} {api_model.name}\n")
         # for m in model_ids:
         #     print(m.name)
+        # for field in api_fields:
+        #     print(field.name)
 
-        for field in api_fields:
-            print(field.name)
         data = json.dumps(model_ids.read([field.name for field in api_fields]))
         return request.make_response(data, headers)
-        # return Response(json.dumps(model_ids.read(["name"])), headers=headers)
-
-        # response = f"<h1>{api.name}</h1><p>Model: {api_model.name}</p><ul>"
-        # for m in model_ids:
-        #     response += f"<li>{m.name}</li>"
-        # response += "</ul>"
-        # return(response)
-
-    def pack_data(self):
-        pass
