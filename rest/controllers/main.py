ee2fbc769d65d058c8fe202bbc76128a18cf1da5
nimi-odoo <nimi@odoo.com>
2022-07-06 11:34:24 -0400

    [IMP] Going to url path shows all model items

--- rest/controllers/main.py
+++ rest/controllers/main.py
@@ -4,17 +4,20 @@ from odoo import http
 from odoo.http import request
 
 class RestController(http.Controller):
-    @http.route('/api_endpoint', auth="public")
+    @http.route('/api/<string:str>', auth="public")
     def index(self, **kw):
-        response = "<ul>"
-        # all_items = http.request.env["rest"].model.search([])
-        # for item in all_items:
-        #     print(item)
-        print("\n\n\ntest\n\n\n")
-        model_ids = http.request.env["ir.model"].search([])
+        url_path = kw["str"]
+        api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
+        api_model = api.specified_model_id
+        model_ids = http.request.env[api_model.model].search([])
+
+        print(f"\napi {url_path} {api_model.name}\n")
+        for m in model_ids:
+            print(m.name)
+
+        response = f"<h1>{api.name}</h1><p>Model: {api_model.name}</p><ul>"
         for m in model_ids:
             response += f"<li>{m.name}</li>"
         response += "</ul>"
         return(response)
-
 
