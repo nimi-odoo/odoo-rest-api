57c48e2468b9d3899081950cff5156a85e1b25b4
“Sunhyeok <suju@odoo.com>
2022-07-07 09:40:59 -0400

    [ADD] RestController : added a 404 handling function

--- rest/controllers/main.py
+++ rest/controllers/main.py
@@ -2,17 +2,30 @@
 
 from odoo import http
 from odoo.http import request
+from odoo.exceptions import ValidationError
 
 import json
 
 class RestController(http.Controller):
+
+    def return_404_json(self):
+        headers = [("Content-Type", "application/json")]
+        jsons = {'status' : 404, "message" : "Invalid endpoint"}
+        res = json.dumps(jsons)
+        return request.make_response(res, headers)
+
     @http.route('/api/<string:str>/', auth="user")
     def index(self, **kw):
+
         headers = [("Content-Type", "application/json")]
         print("Headers\n", headers)
 
         url_path = kw["str"]
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
+
+        if not api.ids:
+            return self.return_404_json()
+
         api_model = api.specified_model_id
         api_fields = api.field_ids
         model_ids = http.request.env[api_model.model].search([])
@@ -33,12 +46,18 @@ class RestController(http.Controller):
 
         url_path = kw["str"]
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
+
+        if not api.ids:
+            return self.return_404_json()
+
         api_model = api.specified_model_id
         api_fields = api.field_ids
         model_id = http.request.env[api_model.model].search([("id", "=", kw["id"])])
 
+        if not model_id.id:
+            return self.return_404_json()
 
 
         data = json.dumps(model_id.read([field.name for field in api_fields]))
         return request.make_response(data, headers)
-        
+
