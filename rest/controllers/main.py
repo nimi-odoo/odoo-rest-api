db2fbfcd9903e24dd6524c119efa3aad1021acad
“Sunhyeok <suju@odoo.com>
2022-07-07 10:34:09 -0400

    commit before pull

--- rest/controllers/main.py
+++ rest/controllers/main.py
@@ -9,6 +9,8 @@ class RestController(http.Controller):
     @http.route('/api/<string:str>/', auth="user", csrf= False)
     def index(self, **kw):
         request_method = http.request.httprequest.headers.environ['REQUEST_METHOD']
+
+
         headers = [("Content-Type", "application/json")]
         print("Headers\n", headers)
 
@@ -40,7 +42,7 @@ class RestController(http.Controller):
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
 
         if not api.ids:
-            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")return self.return_404_json()
+            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")
 
         api_model = api.specified_model_id
         api_fields = api.field_ids
@@ -52,4 +54,6 @@ class RestController(http.Controller):
 
         data = json.dumps(model_id.read([field.name for field in api_fields]))
         return request.make_response(data, headers)
+
+    def post(self, arg):
 
