b500b39979adedafc61594d34abd8b5c7e6975f9
nimi-odoo <nimi@odoo.com>
2022-07-08 15:11:59 -0400

    [REM] Removed overridden default_get

--- rest/models/rest.py
+++ rest/models/rest.py
@@ -66,9 +66,3 @@ class Rest(models.Model):
             build += "\n}"
             record.schema = build
 
-
-    @api.model
-    def default_get(self, fields=[]):
-        print("getting, fields:")
-        vars = super(Rest, self).default_get(fields)
-        for v in vars: print(v)
