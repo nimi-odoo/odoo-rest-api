1947ea7396e79b763d56afee4b7aab3cb8501041
nimi-odoo <nimi@odoo.com>
2022-07-11 13:20:40 -0400

    [REF] Delete and get one methods, error handling

--- rest/controllers/main.py
+++ rest/controllers/main.py
@@ -4,8 +4,8 @@ from odoo import http
 from odoo.http import request, Response
 
 import json
+from json.decoder import JSONDecodeError
 from odoo.exceptions import ValidationError, UserError, AccessError
-
 
 
 class RestController(http.Controller):
@@ -13,70 +13,78 @@ class RestController(http.Controller):
     @http.route('/api/<string:str>/', auth="user", csrf= False)
     def index(self, **kw):
         request_method = http.request.httprequest.headers.environ['REQUEST_METHOD']
-        headers = [("Content-Type", "application/json")]
-        print("Headers\n", headers)
 
         if request_method == "GET":
-            return self.get(kw)
+            return self.get(**kw)
         elif request_method == "POST":
-            return self.post(kw)
+            return self.post(**kw)
         elif request_method == "DELETE" or request_method == "UNLINK":
-            return self.remove(kw)
+            return self.remove(**kw)
         elif request_method == "PUT" or request_method == "PATCH":
-            return self.update(kw)
+            return self.update(**kw)
         else:
-            print("WHY")
-        return self.get(kw)
+            return self.response_404("Invalid request method")
 
 
     @http.route('/api/<string:str>/<int:id>', auth="user", csrf= False)
+    def index_id(self, **kw):
+        request_method = http.request.httprequest.headers.environ['REQUEST_METHOD']
+
+        if request_method == "GET":
+            return self.get_one(**kw)
+        elif request_method == "DELETE" or request_method == "UNLINK":
+            return self.delete_one(**kw)
+        else: 
+            return self.response_404("Invalid request method")
+
+
     def get_one(self, **kw):
         headers = [("Content-Type", "application/json")]
-        print("Headers\n", headers)
-
-        url_path = kw["str"]
-        api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
-
-        if not api.ids:
-            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")
-
-        api_model = api.specified_model_id
-        api_fields = api.field_ids
-        model_id = http.request.env[api_model.model].search([("id", "=", kw["id"])])
-
-        if not model_id.id:
-            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")
-
-
-        data = json.dumps(model_id.read([field.name for field in api_fields]))
+        record_id = self.retrieve_record_id(**kw)
+        if not record_id:
+            return self.response_404("Record not found. The path or id may not exist.")
+        data = self.retrieve_record_data(record_id, **kw)
         return request.make_response(data, headers)
 
 
-    def get(self, kw):
+    def delete_one(self, **kw):
+        headers = [("Content-Type", "application/json")]
+        record_id = self.retrieve_record_id(**kw)
+        if not record_id:
+            return self.response_404("Record not found. The path or id may not exist.")
+        data = self.retrieve_record_data(record_id, **kw)
+        record_id.unlink()
+        return request.make_response(data, headers)
+
+
+    def get(self, **kw):
         headers = [("Content-Type", "application/json")]
         url_path = kw["str"]
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
 
         if not api.ids:
-            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")
+            return self.response_404("Record not found. The path or id may not exist.")
 
         api_model = api.specified_model_id
         api_fields = api.field_ids
         model_ids = http.request.env[api_model.model].search([])
 
-        data = json.dumps(model_ids.read([field.name for field in api_fields]))
+        data = json.dumps(model_ids.read([field.name for field in api_fields]), default=str)
         return request.make_response(data, headers)
 
 
-    def post(self, kw):
+    def post(self, **kw):
         headers = [("Content-Type", "application/json")]
-        data = json.loads(http.request.httprequest.data)
+        try:
+            data = json.loads(http.request.httprequest.data)
+        except JSONDecodeError as e:
+            return self.response_404(f"Invalid JSON formatting\n{e}")
 
         url_path = kw["str"]
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
 
         if not api.ids:
-            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")
+            return self.response_404("Record not found. The path or id may not exist.")
 
         api_model = api.specified_model_id
         api_fields = api.field_ids
@@ -88,6 +96,7 @@ class RestController(http.Controller):
         required_field_names = [f.name for f in required_fields]
         default_fields = http.request.env[api_model.model].default_get(required_field_names)
 
+
         unfulfilled_needed_fields = [f for f in unfulfilled_required_fields if f.name not in default_fields]
         if unfulfilled_needed_fields:
             return self.response_400(f"The following fields are required: {[f.name for f in unfulfilled_needed_fields]}")
@@ -96,7 +105,46 @@ class RestController(http.Controller):
             request.env[api_model.model].create(data)
         except ValueError:
             return self.response_400(ValueError)
-        return request.make_response(json.dumps(data), headers)
+        except:
+            return self.response_400()
+        return request.make_response(json.dumps(data, default=str), headers)
+
+
+
+    def response_400(self, body="400 Bad Request", mimetype="text/plain"):
+        return Response(response=body, status=400, mimetype=mimetype)
+
+
+    def response_404(self, body="404 Not Found", mimetype="text/plain"):
+        return Response(response=body, status=404, mimetype=mimetype)
+
+
+    def response_200(self, body="200 OK", mimetype="application/json"):
+        return Response(response=body, status=200, mimetype=mimetype)
+
+
+    def retrieve_record_id(self, **kw):
+        url_path = kw["str"]
+        api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
+
+        if not api.ids:
+            return False
+
+        api_model = api.specified_model_id
+        record_id = http.request.env[api_model.model].search([("id", "=", kw["id"])])
+
+        if not record_id.id:
+            return False
+        return record_id
+
+    
+    def retrieve_record_data(self, record_id, **kw):
+        url_path = kw["str"]
+        api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
+        api_fields = api.field_ids
+
+        return (json.dumps(record_id.read([field.name for field in api_fields])[0], default=str))
+
 
     def remove(self, kw):
         headers = [("Content-Type", "application/json")]
@@ -106,13 +154,13 @@ class RestController(http.Controller):
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
 
         if not api.ids:
-            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")
+            return self.response_404("Record not found. The path or id may not exist.")
 
         api_model = api.specified_model_id
 
         try:
             if not "id" in data:
-                raise UserError(f'We need id')
+                raise UserError(f'The id of the record to be deleted must be provided')
 
             record_id_to_delete = data["id"]
 
@@ -128,18 +176,9 @@ class RestController(http.Controller):
             else:
                 record.unlink()
         except (UserError, ValidationError, AccessError) as e:
-            return self.response_400(self, str(e))
+            return self.response_400(str(e))
 
-        return request.make_response(json.dumps({"message": "Succesfully delete a record."}), headers)
-
-
-    def response_400(self, message="400 Bad Request", mimetype="text/plain"):
-        return Response(response=message, status=400, mimetype=mimetype)
-
-    def response_404(self, message="404 Not Found", mimetype="text/plain"):
-        return Response(response=message, status=404, mimetype=mimetype)
-
-
+        return request.make_response(json.dumps(data, default=str), headers)
 
 
     # Still working on
@@ -151,7 +190,7 @@ class RestController(http.Controller):
         api = http.request.env["rest.endpoint"].search([("model_path_url", "=", url_path)])
 
         if not api.ids:
-            return request.not_found("Page not found.\n Check your url path or the id you entered exists.")
+            return self.response_404("Record not found. The path or id may not exist.")
 
         api_model = api.specified_model_id
 
