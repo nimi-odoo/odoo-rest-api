a87854fd7079912f2c853cf504bd7beafc9fb3c9
“Sunhyeok <suju@odoo.com>
2022-07-12 14:53:48 -0400

    [FIX] Now authentification works on both third-party apis and http

--- rest/models/ir_http.py
+++ rest/models/ir_http.py
@@ -7,16 +7,17 @@ from odoo import SUPERUSER_ID
 
 class IrHttp(models.AbstractModel):
     _inherit = "ir.http"
-
-    # TODO :
-    # change print() into raise()
     @classmethod
     def _auth_method_check_api_key(cls):
+        is_admin = False
+        if request.httprequest.session.uid:
+            uid = request.httprequest.session.uid
+            request.uid = uid # Move login information from odoo to our http.request
 
-        uid = request.httprequest.session.uid
-        is_admin = request.env['res.users'].search([('id', '=', uid)])._is_admin() # "res.users"._is_superuser() or "res.users".has_group('base.group_erp_manager')
-        print("Is Admin? :", is_admin)
-        if not is_admin:
+            # "res.users"._is_superuser() or "res.users".has_group('base.group_erp_manager')
+            # This is for later reference, it is not in use currently.
+            is_admin = request.env['res.users'].search([('id', '=', uid)])._is_admin()
+        else:
             api_key = request.httprequest.headers.get("Authorization")
             if not api_key:
                 raise AccessError("Authorization header with API key missing")
@@ -26,5 +27,3 @@ class IrHttp(models.AbstractModel):
                     raise AccessError("Invalid API Key")
                 else:
                     request.uid = user_id
-        else:
-            request.uid = uid
