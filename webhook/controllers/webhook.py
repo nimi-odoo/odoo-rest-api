f49ae4ede2cd9ea4543c95c6df1427feb7387b11
“Sunhyeok <suju@odoo.com>
2022-08-03 09:47:00 -0400

    [ADD] Added new endpoint /webhook  for webhook controller

--- webhook/controllers/webhook.py
+++ webhook/controllers/webhook.py
@@ -9,55 +9,11 @@ from json.decoder import JSONDecodeError
 from odoo.exceptions import ValidationError, UserError, AccessError
 
 
-#TODO:
-#display errors
-#When user didn't choose webhook
-#When user didn't input webhook_url
 class Webhook(http.Controller):
-    @http.route('/my/webhook/', type='http', auth="user", website=True, methods=['GET', 'POST'])
-    def index(self, **post):
-        webhooks = request.env['base.automation'].sudo().search([('is_webhook','=','True')])
-        webhook_subscriptions = request.env.user.webhook_subscriptions.search([],order = "create_date desc")
-        selected_webhooks = []
-        values = {"webhooks" : webhooks, "webhook_subscriptions" : [a for a in webhook_subscriptions], "selected_webhooks" : []}
+    @http.route('/my/webhook/', type='http', auth="user", website=True)
+    def MyWebhook(self, **post):
+        return request.render("webhook.portal_my_webhook")
 
-        if request.httprequest.method == "POST":
-            if "submit" in post:
-                uid = post.get('uid').strip()
-                webhook_url = post.get('webhook_url').strip()
-
-                if 'previous_selected_webhooks' in post and post.get('previous_selected_webhooks') != "":
-                    previous_selected_webhooks = ast.literal_eval(post.get('previous_selected_webhooks'))
-                else:
-                    previous_selected_webhooks = []
-
-                if webhook_url == "" or previous_selected_webhooks == []:
-                    raise ValidationError("Please fill all field.")
-                else:
-                    self.subscribe(uid, webhook_url, previous_selected_webhooks)
-            else:
-                if "addRow" in post:
-                    if 'previous_selected_webhooks' in post and post.get('previous_selected_webhooks') != "":
-                        previous_selected_webhooks = ast.literal_eval(post.get('previous_selected_webhooks'))
-                    else:
-                        previous_selected_webhooks = []
-                    previous_selected_webhooks.append(post['webhook_id'])
-                    updated_webhooks = webhooks.search([('is_webhook','=','True'),('id','not in', previous_selected_webhooks)])
-                    values['webhooks'] = updated_webhooks
-                    values["selected_webhooks"] = previous_selected_webhooks
-                elif "deleteRow" in post:
-                    if 'previous_selected_webhooks' in post and post.get('previous_selected_webhooks') != "":
-                        previous_selected_webhooks = ast.literal_eval(post.get('previous_selected_webhooks'))
-                    previous_selected_webhooks.remove(post['selected_webhook_row_index'])
-                    updated_webhooks = webhooks.search([('is_webhook','=','True'),('id','not in', previous_selected_webhooks)])
-                    values['webhooks'] = updated_webhooks
-                    values["selected_webhooks"] = previous_selected_webhooks
-                    print(post)
-
-
-        return request.render("webhook.portal_my_webhook", values, headers={
-            'X-Frame-Options': 'DENY'
-        })
     def subscribe(self, uid, webhook_url, webhook_ids):
         webhook_ids = [(4,int(webhook_id)) for webhook_id in webhook_ids]
         vals = {
@@ -67,7 +23,7 @@ class Webhook(http.Controller):
         }
         request.env['webhook_subscription'].sudo().create(vals)
 
-    def deleteRow(self):
-        print("ASD")
-
+    @http.route('/webhook', auth="public", csrf= False)
+    def index(self, **kw):
+        request_method = http.request.httprequest.headers.environ['REQUEST_METHOD']
 
