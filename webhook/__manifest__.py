98447eea9cffc9732ec586ba1ca7a28c95f5c22b
“Sunhyeok <suju@odoo.com>
2022-08-16 10:39:51 -0400

    [IMP] changed record rules

--- webhook/__manifest__.py
+++ webhook/__manifest__.py
@@ -16,7 +16,8 @@
     'depends': ['website','base','base_automation','rest'],
     'data': [
         "security/ir.model.access.csv",
-        "security/security.xml",
+        "security/webhook_log_security.xml",
+        "security/webhook_subscription_security.xml",
         "views/webhook_menus.xml",
         "views/portal_template.xml",
         "views/base_automation_view.xml",
